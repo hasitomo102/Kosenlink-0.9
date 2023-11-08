84dfcf9bc55ed662818d26b87c493d03dcea3976
kekoawong <kwong6@nd.edu>
2023-11-08 18:59:06 -0500

    Added postgres and auth adapter

--- app/auth.ts
+++ app/auth.ts
@@ -1,10 +1,23 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from 'pg'
+
+// https://authjs.dev/reference/adapter/pg
+const pool = new Pool({
+  host: 'localhost',
+  user: 'database-user',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
 export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
+  adapter: PostgresAdapter(pool),
+  // https://authjs.dev/getting-started/providers/email-tutorial
   providers: [
     // GitHub({
     //   clientId: process.env.OAUTH_CLIENT_KEY as string,
