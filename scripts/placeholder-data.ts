e37c4064216f28bc1deba541cfe468de62534ccd
kekoawong <kwong6@nd.edu>
2023-11-08 18:01:31 -0500

    Finished user types

--- scripts/placeholder-data.ts
+++ scripts/placeholder-data.ts
@@ -1,74 +1,77 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+
+import { User } from "@/types/user";
+
 // https://nextjs.org/learn/dashboard-app/fetching-data
-const users = [
+const users: User[] = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
     email: 'user@nextmail.com',
-    password: '123456',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Felix'
   },
 ];
 
-const customers = [
+const customers: User[] = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: 'Delba de Oliveira',
     email: 'delba@oliveira.com',
-    image_url: '/customers/delba-de-oliveira.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Delba'
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
     name: 'Lee Robinson',
     email: 'lee@robinson.com',
-    image_url: '/customers/lee-robinson.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lee'
   },
   {
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
     name: 'Hector Simpson',
     email: 'hector@simpson.com',
-    image_url: '/customers/hector-simpson.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Hector'
   },
   {
     id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
     name: 'Steven Tey',
     email: 'steven@tey.com',
-    image_url: '/customers/steven-tey.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Steven'
   },
   {
     id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
     name: 'Steph Dietz',
     email: 'steph@dietz.com',
-    image_url: '/customers/steph-dietz.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Steph'
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
     name: 'Michael Novotny',
     email: 'michael@novotny.com',
-    image_url: '/customers/michael-novotny.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Michael'
   },
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
     name: 'Evil Rabbit',
     email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Evil'
   },
   {
     id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
     name: 'Emil Kowalski',
     email: 'emil@kowalski.com',
-    image_url: '/customers/emil-kowalski.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Emil'
   },
   {
     id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
     name: 'Amy Burns',
     email: 'amy@burns.com',
-    image_url: '/customers/amy-burns.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Amy'
   },
   {
     id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
     name: 'Balazs Orban',
     email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
+    imageUrl: 'https://api.dicebear.com/7.x/fun-emoji/svg?seed=Balazs'
   },
 ];
 
