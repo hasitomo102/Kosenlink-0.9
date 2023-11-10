9f4b7aaa6bf783b8b14e617add9220f3bbbac12d
kekoawong <kwong6@nd.edu>
2023-11-10 15:50:52 -0500

    Auth now working

--- app/auth.ts
+++ app/auth.ts
@@ -10,33 +10,11 @@ import { firestore } from '@/app/lib/firebase';
 // http guide: https://authjs.dev/guides/providers/email-http
 // smtp guide: https://next-auth.js.org/providers/email
 
-// https://authjs.dev/reference/adapter/pg
-// const pool = new Pool();
-
-export default NextAuth({
-  // adapter: PostgresAdapter(pool),
+export const AuthOptions = {
   adapter: FirestoreAdapter(firestore),
   secret: process.env.AUTH_SECRET,
   // https://authjs.dev/getting-started/providers/email-tutorial
   providers: [
-    // {
-    //   id: 'email',
-    //   type: 'email',
-    //   // server: {
-    //   //   host: process.env.SMTP_HOST,
-    //   //   port: Number(process.env.SMTP_PORT),
-    //   //   auth: {
-    //   //     user: process.env.SMTP_USER,
-    //   //     pass: process.env.SMTP_PASSWORD,
-    //   //   },
-    //   // },
-    //   // from: process.env.EMAIL_FROM as string,
-    // },
-    // Github({
-    //   clientId: process.env.OAUTH_CLIENT_KEY as string,
-    //   clientSecret: process.env.OAUTH_CLIENT_SECRET as string
-    // }),
-    // }
     Email({
       server: {
         host: process.env.SMTP_HOST,
@@ -49,9 +27,6 @@ export default NextAuth({
       from: process.env.EMAIL_FROM,
     }),
   ],
-  // https://authjs.dev/guides/basics/pages
-  // pages: {
-  //   signIn: '/sign-in',
-  //   verifyRequest: "/verify-request",
-  // }
-});
+};
+
+export default NextAuth(AuthOptions);
--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,9 @@
+import { AuthOptions } from '@/app/auth';
 import Navbar from '@/app/navbar';
 import { getServerSession } from 'next-auth';
 
 export default async function Nav() {
-  const session = await getServerSession();
+  const session = await getServerSession(AuthOptions);
   console.log("user session", session);
   return <Navbar user={session?.user} />;
 }
