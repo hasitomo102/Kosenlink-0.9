d2f44dffe47faabe326822a4842c0e3f79c2f042
kekoawong <kwong6@nd.edu>
2023-11-10 16:07:39 -0500

    Added the necessary auth package

--- app/auth.ts
+++ app/auth.ts
@@ -1,15 +1,10 @@
 import NextAuth from 'next-auth';
-// import Github from "next-auth/providers/github";
 import Email from "next-auth/providers/email"
-import PostgresAdapter from "@auth/pg-adapter";
-// import PostgresAdapter from '@/app/lib/postgres-adaptor';
-// import { Pool } from 'pg';
 import { FirestoreAdapter } from "@auth/firebase-adapter"
 import { firestore } from '@/app/lib/firebase';
 
 // http guide: https://authjs.dev/guides/providers/email-http
 // smtp guide: https://next-auth.js.org/providers/email
-
 export const AuthOptions = {
   adapter: FirestoreAdapter(firestore),
   secret: process.env.AUTH_SECRET,
