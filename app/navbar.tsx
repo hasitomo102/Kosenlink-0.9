2e4b3425311e7c854615f13aa2a13f9abaca7d75
kekoawong <kwong6@nd.edu>
2023-11-15 14:42:53 -0500

    Added sign in feature

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,6 +7,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 import { User } from '@/types/user';
+import Link from 'next/link';
 
 const navigation = [
   { name: 'Dashboard', href: '/' },
@@ -70,59 +71,35 @@ export default function Navbar({ user }: { user?: Partial<User> }) {
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
                 <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
+                {user ? (
+                <>
+                  <Link href="/profile" className="flex items-center px-4">
+                    <div className="mr-3">
+                      <div className="text-sm font-medium text-gray-500">
+                        {user.email}
+                      </div>
+                    </div>
+                    <div className="flex-shrink-0">
                       <Image
                         className="h-8 w-8 rounded-full"
-                        src={user?.image || 'https://avatar.vercel.sh/leerob'}
+                        src={user.image || "/favicon.ico"}
                         height={32}
                         width={32}
-                        alt={`${user?.name || 'placeholder'} avatar`}
+                        alt={`${user.name} avatar`}
                       />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
+                    </div>
+                  </Link>
+                </>
+              ) : (
+                <div className="mt-3 space-y-1">
+                  <button
+                    onClick={() => signIn('email')}
+                    className="flex w-full px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {user ? (
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
-                              )}
-                              onClick={() => signOut()}
-                            >
-                              Sign out
-                            </button>
-                          )}
-                        </Menu.Item>
-                      ) : (
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
-                              )}
-                              onClick={() => signIn('email')}
-                            >
-                              Sign in
-                            </button>
-                          )}
-                        </Menu.Item>
-                      )}
-                    </Menu.Items>
-                  </Transition>
+                    Sign in
+                  </button>
+                </div>
+              )}
                 </Menu>
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
@@ -160,33 +137,22 @@ export default function Navbar({ user }: { user?: Partial<User> }) {
             <div className="border-t border-gray-200 pt-4 pb-3">
               {user ? (
                 <>
-                  <div className="flex items-center px-4">
+                  <Link href="/profile" className="flex items-center px-4">
                     <div className="flex-shrink-0">
                       <Image
                         className="h-8 w-8 rounded-full"
-                        src={user.image || ""}
+                        src={user.image || "/favicon.ico"}
                         height={32}
                         width={32}
                         alt={`${user.name} avatar`}
                       />
                     </div>
                     <div className="ml-3">
-                      <div className="text-base font-medium text-gray-800">
-                        {user.name}
-                      </div>
                       <div className="text-sm font-medium text-gray-500">
                         {user.email}
                       </div>
                     </div>
-                  </div>
-                  <div className="mt-3 space-y-1">
-                    <button
-                      onClick={() => signOut()}
-                      className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-                    >
-                      Sign out
-                    </button>
-                  </div>
+                  </Link>
                 </>
               ) : (
                 <div className="mt-3 space-y-1">
