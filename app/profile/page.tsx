56df9eaf11a9ea135a34048814b7291396b517a1
kekoawong <kwong6@nd.edu>
2023-11-27 17:18:13 -0500

    Added form action on client component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@ import SignOutButton from "@/app/components/signout-button";
 import SubmitButton from "@/app/components/submit-form-button";
 import { auth } from "@/app/lib/auth";
 import { getUserWithEmail, updateUser } from "@/app/lib/users";
+import { TextInput } from "@tremor/react";
 import { z } from "zod";
 
 /**
@@ -50,28 +51,28 @@ export default async function Profile() {
       <main className="p-4 md:p-10 mx-auto max-w-7xl">
         {/* <ProfileForm user={user} /> */}
         <form action={submitFormAction}>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
+            <label className="block text-sm font-medium leading-6 text-gray-900 mt-2">
             First Name
             </label>
-            <div className="mt-2">
-            <input
+            <TextInput
                 name="firstName"
                 id="firstName"
+                required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder={user?.firstName || "First Name"}
+                defaultValue={user?.firstName}
             />
-            </div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
+            <label className="block text-sm font-medium leading-6 text-gray-900 mt-2">
             Last Name
             </label>
-            <div className="mt-2">
-            <input
+            <TextInput
                 name="lastName"
                 id="lastName"
+                required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder={user?.lastName || "Last Name"}
+                defaultValue={user?.lastName}
             />
-            </div>
             <SubmitButton loadingText="Updating...">Update User</SubmitButton>
             <SignOutButton redirectURL="/" />
       </form>
