a944c6407a7c8a51b17875275227b41574a9b633
kekoawong <kwong6@nd.edu>
2023-12-01 18:40:54 -0500

    Added invite form on profile screen

--- app/profile/invite.tsx
+++ app/profile/invite.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { User } from "@/types/user";
-import { CheckCircleIcon, SparklesIcon } from "@heroicons/react/24/solid";
-import { Button, Text, TextInput } from "@tremor/react";
+import { CheckCircleIcon, PaperAirplaneIcon, SparklesIcon } from "@heroicons/react/24/solid";
+import { Button, Text, TextInput, Title } from "@tremor/react";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
@@ -10,17 +10,17 @@ import { z } from "zod";
 import { fromZodError } from 'zod-validation-error';
 
 // set schema
-const LoginEmail = z.string().email("Please enter a valid email address.");
+const InvitedEmail = z.string().email("Please enter a valid email address.");
 
 /**
- * Sign in page using the next auth framework
+ * Basic invite page screen for new invites
  * https://authjs.dev/guides/basics/pages?frameworks=core#email-sign-in
  * Can also make use of a redirect: https://next-auth.js.org/getting-started/client#using-the-redirect-false-option
  *
  * @export
  * @return {*} 
  */
-export default function SignIn() {
+export default function InviteUsers() {
   // define states
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -44,11 +44,11 @@ export default function SignIn() {
   const callbackUrl = useSearchParams().get("callbackUrl") || "/";
 
   // handle sign in function
-  const handleSignIn = async () => {
+  const handleInvite = async () => {
     setLoading(true);
     // parse the data
     try {
-      const parsedEmail = LoginEmail.parse(email);
+      const parsedEmail = InvitedEmail.parse(email);
 
       // check if user is in database
       const emailParams = new URLSearchParams();
@@ -59,11 +59,11 @@ export default function SignIn() {
       // if it is a new user, set the referring url in the parameters for the profile screen
       if (!userData) {
         const newCallbackUrl = createProfileCallbackUrl(callbackUrl);
-        await signIn('email', { email: parsedEmail, callbackUrl: newCallbackUrl, redirect: false });
+        // await signIn('email', { email: parsedEmail, callbackUrl: newCallbackUrl, redirect: false });
         setSuccess(true);
       } else {
         // return the normal callback url if user already has an account
-        await signIn('email', { email: parsedEmail, callbackUrl, redirect: false });
+        // await signIn('email', { email: parsedEmail, callbackUrl, redirect: false });
         setSuccess(true);
       }
     } catch (e: any) {
@@ -74,18 +74,13 @@ export default function SignIn() {
   };
 
   return (
-    <div className="flex-grow mx-auto w-full max-w-lg p-6">
-      <div className="mx-auto w-full max-w-lg p-6">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Enter your email to sign in
-        </h2>
-        <TextInput type="email" disabled={success || loading} onSelect={() => setError("")} onValueChange={(val) => setEmail(val)} className="mt-4" placeholder="Enter your email" />
-        <Button icon={!success ? SparklesIcon : CheckCircleIcon} disabled={success || loading} aria-disabled={loading || success} loading={loading} onClick={handleSignIn} className="mt-4 w-full">
-          {!success ? "Send magic sign-in link" : "Magic sign-in link sent!"}
+    <div className="mt-10">
+        <Title className="mb-1 ml-1">Invite New Users</Title>
+        <TextInput type="email" disabled={success || loading} onSelect={() => setError("")} onValueChange={(val) => setEmail(val)} placeholder="Enter Email" />
+        <Button onClick={handleInvite} icon={PaperAirplaneIcon} aria-disabled={loading} loading={loading} className="mt-4 w-full">
+          Invite User
         </Button>
-        <Text className="mt-2 text-center" color="red">{error}</Text>
-        {success ? <Text className="mt-4 w-full text-center">Open your email and click on the magic link to sign in.</Text> : null}
-      </div>
+        <Text className="mt-1 text-center" color="red">{error}</Text>
     </div>
   );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,9 +2,10 @@ import SignOutButton from "@/app/components/signout-button";
 import SubmitButton from "@/app/components/submit-form-button";
 import { auth } from "@/app/auth/config";
 import { getUserWithEmail, updateUser } from "@/app/lib/users";
-import { Subtitle, Text, TextInput, Title } from "@tremor/react";
+import { Text, TextInput } from "@tremor/react";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import InviteUsers from "@/app/profile/invite";
 
 /**
  * Define the form schema for zod form validation
@@ -62,7 +63,6 @@ export default async function Profile({ searchParams }: { searchParams: { callba
                 name="firstName"
                 id="firstName"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder={user?.firstName || "First Name"}
                 defaultValue={user?.firstName}
             />
@@ -71,7 +71,6 @@ export default async function Profile({ searchParams }: { searchParams: { callba
                 name="lastName"
                 id="lastName"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder={user?.lastName || "Last Name"}
                 defaultValue={user?.lastName}
             />
@@ -81,6 +80,7 @@ export default async function Profile({ searchParams }: { searchParams: { callba
             {!searchParams.callbackUrl ? <SignOutButton className="w-full mt-4" type="button" variant="secondary" email={user?.email} /> : null}
             
       </form>
+      <InviteUsers />
       </div>
     </div>
     )
